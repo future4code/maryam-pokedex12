59125b74935457681a6c7f01ec227e7c26c7c129
mcamilli <marcelo.camilli91@gmail.com>
2021-10-18 15:49:26 -0300

    Adicionei o Layout da pagina Home e os arquivos de API

--- projeto/src/Components/Home.js
+++ projeto/src/Components/Home.js
@@ -1,8 +1,12 @@
-import react from "react"
+import react, { useEffect, useState } from "react"
 import { useHistory } from "react-router-dom"
+import { CardContainer, HomeContainer, PokeButton } from "./styled"
+import axios from "axios"
 
 const Home = () =>  {
     const history = useHistory()
+    const [pokemons, setPokemons] = useState ([])
+   
 
     const pageList = () => {
         history.push("/lista")
@@ -11,15 +15,47 @@ const Home = () =>  {
         history.push("/detalhes")
     }
 
+    const getPokemons = () => {
+    axios
+    .get ("https://pokeapi.co/api/v2/pokemon/")
+    .then ((res) => { 
+        
+        setPokemons(res.data.results)
+    })
+    .catch ((err) => {
+      console.log (err)
+    })
+  }
+
+  useEffect (() => {
+    getPokemons ()
+  }, [])
+
+
+
+  const pokemonList = pokemons.map((pokemon) => {
     return (
+    <CardContainer>
+      <p>{pokemon.name}</p>
+      <PokeButton>
+        <button> Adicionar </button>
+        <button> Detalhes </button>
+      </PokeButton>
+    </CardContainer>
+    )
+  })
+
+    return (
+      <>
       <div>
-
         <button onClick={pageList}>Ir lista de Pokemon</button>
-
-        <button onClick={pageDetalhes}>Ir para Detalhes</button>
-
-        <h1>Pagina home</h1>
+        <button onClick={pageDetalhes}>Ir para Detalhes</button>      
       </div>
+      
+      <HomeContainer>        
+          {pokemonList}           
+      </HomeContainer>
+        </>
     );
   }
   
